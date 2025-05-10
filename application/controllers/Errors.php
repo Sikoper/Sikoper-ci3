555965774db296cc690d11c3231e4e1ff20c9220
FryenX <ardiwidana@gmail.com>
2025-05-10 12:30:43 +0800

    melengkapi fitur CRUD untuk pegawai serta menambahkan fitur check detail agar bisa melihat data lengkap pegawai. serta menambahkan filter nik agar link edit tidak dapat diakses sembarangan. dan akhirnya mengganti error 404 bawaan ci3 menjadi error 404 template serta melakukan hash url untuk nik

---
+++ application/controllers/Errors.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Errors extends CI_Controller
+{
+    public function error_404()
+    {
+        $this->output->set_status_header('404');
+        $this->load->view('errors/error_404');
+        return;
+    }
+}
--- application/views/pegawai/index.php
+++ application/views/pegawai/index.php
@@ -75,4 +75,47 @@
             }
         ],
     });
+
+    function deleteItem(id, nama) {
+        Swal.fire({
+            title: "Hapus data ini?",
+            html: `Yakin ingin menghapus data dari <strong>${nama}</strong>?`,
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes!",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    type: "POST",
+                    url: "<?= base_url('pegawai/delete') ?>",
+                    data: {
+                        id: id
+                    },
+                    dataType: "json",
+                    success: function(response) {
+                        if (response.success) {
+                            Swal.fire({
+                                title: "Success!",
+                                text: response.success,
+                                icon: "success"
+                            }).then((result) => {
+                                if (result.isConfirmed) {
+                                    window.location.reload();
+                                }
+                            });
+                        }
+                    },
+                    error: function(xhr, thrownError) {
+                        alert(xhr.status + "\n" + xhr.responseText + "\n" + thrownError);
+                    }
+                });
+            }
+        });
+    }
+
+    function detail(id) {
+        
+    }
 </script>
