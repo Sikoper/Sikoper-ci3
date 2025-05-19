26b7b705bde1180dd8dc21a5e1e54092e6fc31c9
bid-bos <abidhidayaturrahman@gmail.com>
2025-05-20 00:04:53 +0800

    implementasi tampilan tabel penarikan dengan fitur hapus data

---
+++ application/views/penarikan/index.php
@@ -0,0 +1,130 @@
+<div class="card">
+    <div class="card-header">
+        <h4 class="card-title">
+            <?php if ($this->session->userdata('level') != 'Direktur') : ?>
+            <button class="btn btn-primary" onclick="window.location='<?= base_url('penarikan/add') ?>'">
+                <i class="fa fa-plus-circle"></i> Tambah Data
+            </button>
+            <?php endif; ?>
+        </h4>
+    </div>
+    <div class="card-body">
+        <div class="dataTable-wrapper dataTable-loading no-footer sortable searchable fixed-columns">
+            <div class="dataTable-container">
+                <table class="table table-striped dataTable-table" id="tabel_pegawai">
+                    <thead>
+                        <tr>
+                            <th class="text-center" width="40px">No</th>
+                            <th>Nama Nasabah</th>
+                            <th>Nomor Rekening</th>
+                            <th>Jenis Tabungan</th>
+                            <th>Total Penarikan</th>
+                            <th>Status</th>
+                            <th>#</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </div>
+            <div class="dataTable-bottom">
+                <ul class="pagination pagination-primary float-end dataTable-pagination">
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    table = $('#tabel_penarikan').DataTable({
+        responsive: true,
+        "destroy": true,
+        "processing": true,
+        "serverSide": true,
+        "order": [],
+        autoWidth: false,
+
+        "ajax": {
+            "url": "<?= site_url('penarikan/fetchData') ?>",
+            "type": "POST"
+        },
+
+        "columns": [{
+                "type": "string"
+            },
+            {
+                "type": "string"
+            },
+            {
+                "type": "string"
+            },
+            {
+                "type": "string"
+            },
+            {
+                "type": "string"
+            },
+            {
+                "type": "string"
+            },
+            {
+                "orderable": false
+            }
+        ],
+
+        "columnDefs": [{
+                "targets": 0,
+                "orderable": false,
+                "width": "5%"
+            },
+            {
+                "targets": 5,
+                "orderable": false,
+                "width": "15%"
+            }
+        ],
+    });
+
+    function deleteItem(id, nama) {
+        Swal.fire({
+            title: "Hapus data ini?",
+            html: `Yakin ingin menghapus data dari <strong>${nama}</strong>?`,
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes!",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    type: "POST",
+                    url: "<?= base_url('penarikan/delete') ?>",
+                    data: {
+                        id: id
+                    },
+                    dataType: "json",
+                    success: function(response) {
+                        if (response.success) {
+                            Swal.fire({
+                                title: "Success!",
+                                text: response.success,
+                                icon: "success"
+                            }).then((result) => {
+                                if (result.isConfirmed) {
+                                    window.location.reload();
+                                }
+                            });
+                        }
+                    },
+                    error: function(xhr, thrownError) {
+                        alert(xhr.status + "\n" + xhr.responseText + "\n" + thrownError);
+                    }
+                });
+            }
+        });
+    }
+
+    function detail(id) {
+
+    }
+</script>
